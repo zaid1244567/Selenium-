1edd3662ffa1b73eb5b66a83addac087c7a0517e
zaid1244567 <syedzaid7348@gmail.com>
2023-08-16 12:04:50 +0530

    committing day3(html)

---
+++ programs/day3(html).py
@@ -0,0 +1,41 @@
+#11-08-2023
+"""
+html
+"""
+"""
+*html stands for "hyper text markup lang".
+*html is used to design and develop web-pages.
+(webpage is a collection of web-elements(text field, check box, radio button, tabs, etc)
+*html code is completely developed by tags.
+(tag means a keyword which is enclosed within angular(< >) brackets is called as tags)
+*if we open a tag then it is mandatory to close a tag.
+
+basic structure of html code:
+*****************************
+<html>
+<head>
+<title> title of webpage </title>
+</head>
+<body>
+    ...web-elements...
+</body>
+</html>
+
+steps to create a web-page:
+---------------------------
+*open notepad and type html code
+*save a file with .html extension
+
+web-element             tag-name                attribute
+===========             ========                =========
+username                <input>                 type="text"
+password                <input>                 type="password"
+button                  <input>                 type="submit"
+link                    <a>                     href="url"
+radio button            <input>                 type="radio"
+check box               <input>                 type="checkbox"
+image                   <img>                   src="path of mage"
+dropdown                <select>                id="a1"
+table                   <table>                 border="4"
+text area               <textarea>              id="text area"
+"""
